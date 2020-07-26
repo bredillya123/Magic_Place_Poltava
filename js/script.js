2cf792278576924acddeaec0561476528d36f74c
bredillya123 <bredillya@gmail.com>
2020-07-26 14:23:27 +0300

    Create Burger + full  addaptive Navigation block

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+$(document).ready(function () {
+  $('.menu-btn').on('click', function(e){
+      e.preventDefault;
+      $(this).toggleClass('menu-btn_active');
+      $('.menu-nav__link').toggleClass('menu-nav__link_active');
+      $('.menu-nav').toggleClass('menu-nav_active');
+  })
+})
+
+
