f6c8e9540ce1986e2968cb7d4d252540024e31ae
bredillya123 <bredillya@gmail.com>
2020-07-29 01:42:44 +0300

    block gallery + adap + button load more + magnific popup + fix in ather blocks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 'use strict'
 
 $(document).ready(function () {
+// magnific popup for gallery
+$('.gallery__img').magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
+
+// for burger mobile version
   $('.menu-btn').on('click', function(e){
       e.preventDefault;
       $(this).toggleClass('menu-btn_active');
       $('.menu-nav__link').toggleClass('menu-nav__link_active');
       $('.menu-nav').toggleClass('menu-nav_active');
   })
+
+// for button Show more
+  $('.button__load--more').on('click', function(e){
+    e.preventDefault;
+    // $(this).text("Скрыть");
+    $('.gallery__slide').toggleClass('gallery__slide--active');
+    $('.text__load').toggleClass('text__load--pasive');
+    $('.text__unload').toggleClass('text__unload--active');
+})
 })
 
 
